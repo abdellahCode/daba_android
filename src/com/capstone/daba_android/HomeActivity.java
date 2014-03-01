68ae4d2ac10d5736b37d7c4cdca9e5a1927d00b7
abdellahCode <my.abdellah.benhammou@gmail.com>
2014-03-01 05:28:26 +0000

    first commit.

---
+++ src/com/capstone/daba_android/HomeActivity.java
@@ -0,0 +1,65 @@
+package com.capstone.daba_android;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class HomeActivity extends Activity {
+	Context context;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+		context = this;
+		ImageButton videoRecAct = (ImageButton) findViewById(R.id.videoRecAct);
+		videoRecAct.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent recordingActivity = new Intent(context, MainActivity.class);
+				context.startActivity(recordingActivity);
+				Toast.makeText(getApplicationContext(), "Go get some Dabaz", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		ImageButton feedAct = (ImageButton) findViewById(R.id.feedAct);
+		feedAct.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				getFragmentManager().beginTransaction().replace(R.id.container, new MyfeedFragment()).commit();
+				Toast.makeText(context, "Feed", Toast.LENGTH_SHORT).show();
+				
+			}
+		});
+		
+		ImageButton profileAct = (ImageButton) findViewById(R.id.profileAct);
+		profileAct.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				getFragmentManager().beginTransaction().replace(R.id.container, new PersonalFragment()).commit();
+				Toast.makeText(context, "Profile", Toast.LENGTH_SHORT).show();
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
+
+}
---
+++ src/com/capstone/daba_android/PersonalFragment.java
@@ -0,0 +1,16 @@
+package com.capstone.daba_android;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PersonalFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		View view = inflater.inflate(R.layout.personal_fragment, container, false);
+		return (view);
+	}
+}
